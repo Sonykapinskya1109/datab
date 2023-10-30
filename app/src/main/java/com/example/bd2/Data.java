4c06f55d1353c2b58f617dcb160afe4df2513c79
mobile <anyuta.vagizova@mail.ru>
2023-10-30 14:17:21 +0500

    fgfbf

---
+++ app/src/main/java/com/example/bd2/Data.java
@@ -0,0 +1,14 @@
+package com.example.bd2;
+
+public class Data {
+    String name;
+    String surname;
+    int year;
+
+    public Data(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+}
+
